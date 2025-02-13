aafea5560fc91a9fc615c1b3b989c2b8ac2c97d9
Dennis <kilonzod19@gmail.com>
2025-02-13 13:29:23 +0300

    Initial commit

---
+++ area_of_rectangle/target/debug/area_of_rectangle.d
@@ -0,0 +1,1 @@
+/home/dk/projects/rust_learning_clean/area_of_rectangle/target/debug/area_of_rectangle: /home/dk/projects/rust_learning_clean/area_of_rectangle/src/main.rs
---
+++ area_of_rectangle/target/debug/deps/area_of_rectangle-04b96f067d67c12b.d
@@ -0,0 +1,5 @@
+/home/dk/projects/rust_learning_clean/area_of_rectangle/target/debug/deps/area_of_rectangle-04b96f067d67c12b: src/main.rs
+
+/home/dk/projects/rust_learning_clean/area_of_rectangle/target/debug/deps/area_of_rectangle-04b96f067d67c12b.d: src/main.rs
+
+src/main.rs:
